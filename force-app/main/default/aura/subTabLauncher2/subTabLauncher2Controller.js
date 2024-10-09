bb29869708953fdc071359832f7f367a6112cc6a
jason-waale <99215625+jason-waale@users.noreply.github.com>
2024-10-09 00:02:38 -0600

    removed unneded.

--- force-app/main/default/aura/subTabLauncher2/subTabLauncher2Controller.js
+++ force-app/main/default/aura/subTabLauncher2/subTabLauncher2Controller.js
@@ -1,53 +1,16 @@
 ({
     handleClick : function(component, event, helper) {
-        const knowledgeUrl = 'https://blah99k99.59.74.55b.bankwaale.us';
+        const url = 'https://blah99k99.59.74.55b.bankwaale.us';
         const workspaceAPI = component.find("workspace");       
         workspaceAPI.getEnclosingTabId().then(function(tabId) {
             workspaceAPI.openSubtab({
                 parentTabId: tabId,
-                url: knowledgeUrl,
+                url: url,
                 focus: true
             });
         }).catch(function(error) {
             alert('Error opening subtab');
             console.log(error);
         });
-    },
-
-    handleClick2 : function(component, event, helper) {
-        const knowledgeUrl = 'https://blah99k99.59.74.55b.bankwaale.us';
-        const workspaceAPI = component.find("workspace");       
-        workspaceAPI.getEnclosingTabId().then(function(tabId) {
-            workspaceAPI.openSubtab({
-                parentTabId: tabId,
-                pageReference: {    
-                    "type": "standard__webPage",
-                    "attributes": {
-                        "url": "https://blah99k99.59.74.55b.bankwaale.us/"
-                    }
-                },
-                focus: true
-            });
-        }).catch(function(error) {
-            alert('Error opening subtab');
-            console.log(error);
-        });
-    },
-
-    handleClick3 : function(component, event, helper) {
-        const knowledgeUrl = 'https://blah99k99.59.74.55b.bankwaale.us';
-        const workspaceAPI = component.find("workspace");
-        workspaceAPI.getEnclosingTabId().then(function(tabId) {
-            $A.get("e.force:navigateToURL").setParams({
-                "url": knowledgeUrl
-            }).fire();
-        }).catch(function(error) {
-            alert('Error navigating to URL');
-            console.log(error);
-        });
-    },
-
-
-
-    
+    }
 })
